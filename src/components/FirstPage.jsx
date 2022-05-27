300ac77fca20af522320fbd5ba9157cb07c12978
Catalina Gonzalez <catalinagonzalez1410@gmail.com>
2022-05-27 01:21:20 -0400

    styled-components, data and images added

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+
+import './FirstPage.css';
+import logo from '../images/logo3.png';
 
 const FirstPage = () => {
   return (
-    <div>FirstPage</div>
+    <div>
+      <div>
+        <img src={logo} alt="RM-logo" />
+      <div>
+        <button>ENTER</button>
+      </div>
+    </div>
+    </div>
   )
 }
 
