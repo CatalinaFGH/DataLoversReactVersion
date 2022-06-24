70282e87a6fe948abad9afca1c6d6bcf9bae96bd
Catalina Gonzalez <catalinagonzalez1410@gmail.com>
2022-06-24 00:54:18 -0400

    basename added to the browserRouter to avoid deployment errors

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import DataPage from './components/DataPage/DataPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path="/" element={<FirstPage />}></Route>
       <Route path="/datapage" element={<DataPage />}></Route>
